5f2321e4ef672b7835b9ef022e38432080819e5d
Audrobot <kornickiaa@gmail.com>
2018-04-17 21:24:47 -0600

    add doc ready to smooth scroll

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - 100
-        }, 600, function() {
-      });
+$(document).ready(function(){  
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 0
+          }, 650, function() {
+        });
+      }
     }
-  }
+  });
 });
-
 
 $(document).ready(function(){
 
