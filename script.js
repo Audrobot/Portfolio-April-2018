11d38295d6b9a27e83f6de83686ee4592bb44324
Audrobot <kornickiaa@gmail.com>
2018-04-18 20:56:41 -0600

    decrease smooth scroll speed; remove unnecessary doc ready wrap

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){  
+  
   $('a[href*="#"]')
     .not('[href="#"]')
     .not('[href="#0"]')
@@ -14,28 +15,27 @@ $(document).ready(function(){
           event.preventDefault();
           $('html, body').animate({
             scrollTop: target.offset().top - 0
-          }, 650, function() {
+          }, 550, function() {
         });
       }
     }
   });
-});
-
-$(document).ready(function(){
 
   $(window).scroll(function(){
-      if ($(window).scrollTop() > 100) {
-          $('#scroll').fadeIn('slow');
-      } else {
-          $('#scroll').fadeOut('slow');
-      }
+    if ($(window).scrollTop() > 100) {
+        $('#scroll').fadeIn('slow');
+    } else {
+        $('#scroll').fadeOut('slow');
+    }
   });
 
   $('#scroll').click(function(){
-      $("html, body").animate({scrollTop : 0},1);
+      $("html, body").animate({scrollTop : 0},400);
       return false;
   });
 
 });
+
+
  
   
